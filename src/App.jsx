6c3b93bb9435babbca96f0efbb380a8b29616811
IgnaDev <arcortoon@gmail.com>
2023-08-18 20:19:33 -0300

    Redux, aplicando redux por PRIMERA VEZ.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {increment, decrement, incrementByAmount, decrementByAmount} from './store/slices/counter/counterSlice'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {counter} = useSelector(state => state.counter)
+  const dispatch = useDispatch()
 
   return (
     <>
@@ -16,18 +19,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={()=>dispatch(increment())}>
+          Incrementar
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={()=>dispatch(decrement())}>
+          Decrementar
+        </button>
+        <button onClick={()=>dispatch(incrementByAmount(10))}>
+          Incrementar de 10 en 10
+        </button>
+        <button onClick={()=>dispatch(decrementByAmount(10))}>
+          Decrementar de 10 en 10
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
