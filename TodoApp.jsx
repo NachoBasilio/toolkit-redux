7786c42da375e6b76f940f80a9ea16ca001ac50a
IgnaDev <arcortoon@gmail.com>
2023-08-19 11:24:49 -0300

    RTKQuery

--- TodoApp.jsx
+++ TodoApp.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import { useGetTodoByIdQuery, useGetTodosQuery } from "./src/store/apis/todosApi"
 
 
 export default function TodoApp() {
+    const [todoId, setTodoId] = useState(1)
     //const {data, isLoading, error} = useGetTodosQuery()
-    const {data, isLoading, error} = useGetTodoByIdQuery()
+    const {data, isLoading, error} = useGetTodoByIdQuery(todoId)
 
   return (
     <>
@@ -14,11 +16,19 @@ export default function TodoApp() {
             isLoading && <p>Loading...</p>
         }
 
-        <pre></pre>
+        <pre>{
+            JSON.stringify(data, null, 2)
+            }</pre>
 
         {
             error && <p>{error}</p>
         }
+
+        <button onClick={() => setTodoId(todoId + 1)}>Siguiente Todo</button>
+        {
+            todoId > 1 && <button onClick={() => setTodoId(todoId - 1)}>Anterior Todo</button>
+        }
+
 
         {/* {
             data && data.map((todo) => (
